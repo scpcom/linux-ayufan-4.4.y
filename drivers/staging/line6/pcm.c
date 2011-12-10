e79fd2d05ee01500567653c5292116b8c3d0a92a
Markus Grabner <grabner@icg.tugraz.at>
2011-12-10 02:12:32 +0100

    staging: line6: fixed ALSA/PCM interaction
    commit 6b02a17ee5cd5d200dbe4a285a4e750f70884967 upstream.
    
    The PCM subsystem in the Line6 driver is mainly used for PCM playback and
    capture by ALSA, but also has other tasks, most notably providing a
    low-latency software monitor for devices which don't support hardware
    monitoring (e.g., the TonePort series). This patch makes ALSA "play nicely"
    with the other components, i.e., prevents it from resetting the isochronous
    USB transfer while other PCM tasks (software monitoring) are running.
    
    Signed-off-by: Markus Grabner <grabner@icg.tugraz.at>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    [bwh: Adjust to apply after backported commit 9f613601482c
     'staging: line6: avoid __sync_fetch_and_{and,or}']

--- drivers/staging/line6/pcm.c
+++ drivers/staging/line6/pcm.c
@@ -86,6 +86,12 @@ static DEVICE_ATTR(impulse_period, S_IWUSR | S_IRUGO, pcm_get_impulse_period,
 
 #endif
 
+static bool test_flags(unsigned long flags0, unsigned long flags1,
+		       unsigned long mask)
+{
+	return ((flags0 & mask) == 0) && ((flags1 & mask) != 0);
+}
+
 int line6_pcm_start(struct snd_line6_pcm *line6pcm, int channels)
 {
 	unsigned long flags_old, flags_new;
@@ -95,11 +101,10 @@ int line6_pcm_start(struct snd_line6_pcm *line6pcm, int channels)
 		flags_old = ACCESS_ONCE(line6pcm->flags);
 		flags_new = flags_old | channels;
 	} while (cmpxchg(&line6pcm->flags, flags_old, flags_new) != flags_old);
-
+	
 	line6pcm->prev_fbuf = NULL;
 
-	if (((flags_old & MASK_CAPTURE) == 0) &&
-	    ((flags_new & MASK_CAPTURE) != 0)) {
+	if (test_flags(flags_old, flags_new, MASK_CAPTURE)) {
 		/*
 		   Waiting for completion of active URBs in the stop handler is
 		   a bug, we therefore report an error if capturing is restarted
@@ -108,32 +113,45 @@ int line6_pcm_start(struct snd_line6_pcm *line6pcm, int channels)
 		if (line6pcm->active_urb_in | line6pcm->unlink_urb_in)
 			return -EBUSY;
 
+		if (!(flags_new & MASK_PCM_ALSA_CAPTURE)) {
+			err = line6_alloc_capture_buffer(line6pcm);
+
+			if (err < 0)
+				goto pcm_start_error;
+		}
+
 		line6pcm->count_in = 0;
 		line6pcm->prev_fsize = 0;
 		err = line6_submit_audio_in_all_urbs(line6pcm);
 
 		if (err < 0)
-			goto fail;
+			goto pcm_start_error;
 	}
 
-	if (((flags_old & MASK_PLAYBACK) == 0) &&
-	    ((flags_new & MASK_PLAYBACK) != 0)) {
+	if (test_flags(flags_old, flags_new, MASK_PLAYBACK)) {
 		/*
 		   See comment above regarding PCM restart.
 		 */
 		if (line6pcm->active_urb_out | line6pcm->unlink_urb_out)
 			return -EBUSY;
 
+		if (!(flags_new & MASK_PCM_ALSA_PLAYBACK)) {
+			err = line6_alloc_playback_buffer(line6pcm);
+
+			if (err < 0)
+				goto pcm_start_error;
+		}
+
 		line6pcm->count_out = 0;
 		err = line6_submit_audio_out_all_urbs(line6pcm);
 
 		if (err < 0)
-			goto fail;
+			goto pcm_start_error;
 	}
 
 	return 0;
 
-fail:
+pcm_start_error:
 	do {
 		flags_old = ACCESS_ONCE(line6pcm->flags);
 		flags_new = flags_old & ~channels;
@@ -151,14 +169,18 @@ int line6_pcm_stop(struct snd_line6_pcm *line6pcm, int channels)
 		flags_new = flags_old & ~channels;
 	} while (cmpxchg(&line6pcm->flags, flags_old, flags_new) != flags_old);
 
-	if (((flags_old & MASK_CAPTURE) != 0) &&
-	    ((flags_new & MASK_CAPTURE) == 0)) {
+	if (test_flags(flags_new, flags_old, MASK_CAPTURE)) {
 		line6_unlink_audio_in_urbs(line6pcm);
+
+		if (!(flags_old & MASK_PCM_ALSA_CAPTURE))
+			line6_free_capture_buffer(line6pcm);
 	}
 
-	if (((flags_old & MASK_PLAYBACK) != 0) &&
-	    ((flags_new & MASK_PLAYBACK) == 0)) {
+	if (test_flags(flags_new, flags_old, MASK_PLAYBACK)) {
 		line6_unlink_audio_out_urbs(line6pcm);
+
+		if (!(flags_old & MASK_PCM_ALSA_PLAYBACK))
+			line6_free_playback_buffer(line6pcm);
 	}
 
 	return 0;
@@ -489,17 +511,20 @@ int snd_line6_prepare(struct snd_pcm_substream *substream)
 
 	switch (substream->stream) {
 	case SNDRV_PCM_STREAM_PLAYBACK:
-		line6_unlink_wait_clear_audio_out_urbs(line6pcm);
+		if ((line6pcm->flags & MASK_PLAYBACK) == 0)
+			line6_unlink_wait_clear_audio_out_urbs(line6pcm);
+
 		break;
 
 	case SNDRV_PCM_STREAM_CAPTURE:
-		line6_unlink_wait_clear_audio_in_urbs(line6pcm);
+		if ((line6pcm->flags & MASK_CAPTURE) == 0)
+			line6_unlink_wait_clear_audio_in_urbs(line6pcm);
+
 		break;
 
 	default:
 		MISSING_CASE;
 	}
-
 
 	if (!test_and_set_bit(BIT_PREPARED, &line6pcm->flags)) {
 		line6pcm->count_out = 0;
