84618b50a35a2a8e9cae62df0f8a2f117ad3d828
Ben Hutchings <ben@decadent.org.uk>
2012-01-10 02:59:49 +0000

    cpu: Do not return errors from cpu_dev_init() which will be ignored
    commit 024f78462c3da710642a54939888a92e28704653 upstream.
    
    cpu_dev_init() is only called from driver_init(), which does not check
    its return value.  Therefore make cpu_dev_init() return void.
    
    We must register the CPU subsystem, so panic if this fails.
    
    If sched_create_sysfs_power_savings_entries() fails, the damage is
    contained, so ignore this (as before).
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [bwh: Adjust to apply on top of commit 87590ce6e373 "sysfs/cpu: Add
     vulnerability folder"]

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -2,6 +2,7 @@
  * CPU subsystem support
  */
 
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/sched.h>
@@ -314,18 +315,13 @@ static void __init cpu_register_vulnerabilities(void)
 static inline void cpu_register_vulnerabilities(void) { }
 #endif
 
-int __init cpu_dev_init(void)
+void __init cpu_dev_init(void)
 {
-	int err;
-
-	err = subsys_system_register(&cpu_subsys, cpu_root_attr_groups);
-	if (err)
-		return err;
+	if (subsys_system_register(&cpu_subsys, cpu_root_attr_groups))
+		panic("Failed to register CPU subsystem");
 
 #if defined(CONFIG_SCHED_MC) || defined(CONFIG_SCHED_SMT)
-	err = sched_create_sysfs_power_savings_entries(cpu_subsys.dev_root);
+	sched_create_sysfs_power_savings_entries(cpu_subsys.dev_root);
 #endif
-	if (!err)
-		cpu_register_vulnerabilities();
-	return err;
+	cpu_register_vulnerabilities();
 }
