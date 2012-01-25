5e165f040a08102ba2c1cdfe9dfb6ceb490b2209
Thomas Renninger <trenn@suse.de>
2012-01-26 00:09:11 +0100

    X86: Introduce HW-Pstate scattered cpuid feature
    commit 2f1e097e24defe64a86535b53768f5c8ab0368d1 upstream.
    
    It is rather similar to CPB (boot capability) feature
    and exists since fam10h (can be looked up in AMD's BKDG).
    
    The feature is needed for powernow-k8 to cleanup init functions and to
    provide proper autoloading matching with the new x86cpu modalias
    feature.
    
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Cc: Dave Jones <davej@redhat.com>
    Cc: Borislav Petkov <bp@amd64.org>
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    Acked-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    [bwh: For wheezy, renumber X86_FEATURE_INVPCID_SINGLE and X86_FEATURE_RSB_CTXSW
     to avoid collision with X86_FEATURE_HW_PSTATE]

--- arch/x86/include/asm/cpufeature.h
+++ arch/x86/include/asm/cpufeature.h
@@ -177,8 +177,9 @@
 #define X86_FEATURE_PLN		(7*32+ 5) /* Intel Power Limit Notification */
 #define X86_FEATURE_PTS		(7*32+ 6) /* Intel Package Thermal Status */
 #define X86_FEATURE_DTHERM	(7*32+ 7) /* Digital Thermal Sensor */
-#define X86_FEATURE_INVPCID_SINGLE (7*32+ 8) /* Effectively INVPCID && CR4.PCIDE=1 */
-#define X86_FEATURE_RSB_CTXSW	(7*32+9) /* "" Fill RSB on context switches */
+#define X86_FEATURE_HW_PSTATE	(7*32+ 8) /* AMD HW-PState */
+#define X86_FEATURE_INVPCID_SINGLE (7*32+ 9) /* Effectively INVPCID && CR4.PCIDE=1 */
+#define X86_FEATURE_RSB_CTXSW	(7*32+10) /* "" Fill RSB on context switches */
 
 #define X86_FEATURE_RETPOLINE	(7*32+29) /* "" Generic Retpoline mitigation for Spectre variant 2 */
 #define X86_FEATURE_RETPOLINE_AMD (7*32+30) /* "" AMD Retpoline mitigation for Spectre variant 2 */
