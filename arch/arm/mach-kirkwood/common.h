d483d8e7d9241fd5fa670cb42ad45113fdd3f37d
Jason Cooper <jason@lakedaemon.net>
2012-03-15 00:52:31 +0000

    ARM: kirkwood: fdt: facilitate new boards during fdt migration
    commit 6fa6b8781fbd5e6cd5e313c5e3bdd73b426d8f30 upstream.
    
    Move all dreamplug-specific code out of board-dt.c and into
    board-dreamplug.c.  This way new boards that are added during the
    conversion to fdt don't clutter up board-dt.c.
    
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/common.h
+++ arch/arm/mach-kirkwood/common.h
@@ -51,6 +51,14 @@ void kirkwood_nand_init(struct mtd_partition *parts, int nr_parts, int delay);
 void kirkwood_nand_init_rnb(struct mtd_partition *parts, int nr_parts, int (*dev_ready)(struct mtd_info *));
 void kirkwood_audio_init(void);
 
+/* board init functions for boards not fully converted to fdt */
+#ifdef CONFIG_MACH_DREAMPLUG_DT
+void dreamplug_init(void);
+#else
+static inline void dreamplug_init(void) {};
+#endif
+
+/* early init functions not converted to fdt yet */
 char *kirkwood_id(void);
 void kirkwood_l2_init(void);
 void kirkwood_rtc_init(void);
