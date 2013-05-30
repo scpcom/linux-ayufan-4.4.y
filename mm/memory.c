d89ab116e8b62f4c55f83032911217a4cab3ed3f
Daniel Mentz <danielmentz@google.com>
2013-05-29 21:12:53 -0700

    OpenWRT patch vm_exports

--- mm/memory.c
+++ mm/memory.c
@@ -1393,6 +1393,7 @@ unsigned long zap_page_range(struct vm_area_struct *vma, unsigned long address,
 	tlb_finish_mmu(&tlb, address, end);
 	return end;
 }
+EXPORT_SYMBOL_GPL(zap_page_range);
 
 /**
  * zap_vma_ptes - remove ptes mapping the vma
@@ -3068,6 +3069,7 @@ static inline int check_stack_guard_page(struct vm_area_struct *vma, unsigned lo
 	}
 	return 0;
 }
+EXPORT_SYMBOL_GPL(vmtruncate_range);
 
 /*
  * We enter with non-exclusive mmap_sem (to exclude vma changes,
