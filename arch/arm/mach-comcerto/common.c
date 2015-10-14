d3b52d8016013453b504ffa35845798354ebf87f
Daniel Mentz <danielmentz@google.com>
2015-10-14 14:32:01 -0700

    Anti reboot loop feature
    The anti reboot loop feature is a communication protocol between the
    bootloader and the kernel that allows the bootloader to determine if a
    certain kernel/rootfs image is "good". The way it works is that the
    bootloader writes a certain bit pattern to a previously agreed memory
    location. If a userspace app decides that the kernel has booted ok and
    that the user space environment is sane, it tells the kernel which then
    flips a bit in DRAM to communicate to the bootloader that this image is
    considered "good".  Now, if the system reboots due to some failure
    condition before said bit has been flipped, the bootloader is able to
    detect this situation and switch back to a different kernel/rootfs
    image.
    
    Change-Id: Ib4c0623fbb03849f1cfb077b731c1394e27d83a1

--- arch/arm/mach-comcerto/common.c
+++ arch/arm/mach-comcerto/common.c
@@ -3,6 +3,7 @@
 #include <linux/irqchip.h>
 
 #include <linux/memblock.h>
+#include <linux/antirebootloop.h>
 
 #include <mach/hardware.h>
 
@@ -16,6 +17,10 @@ void __init c2k_reserve(void)
 	if (memblock_reserve(0, 0x24) < 0)
 		BUG();
 
+	if (memblock_reserve((phys_addr_t) get_antirebootloop_ptr(),
+				PAGE_SIZE) < 0)
+		BUG();
+
 	/* Allocate DDR block used by PFE/MSP, the base address is fixed so that util-pe code can
 	be linked at a fixed address */
 	if (memblock_reserve(COMCERTO_DDR_SHARED_BASE, COMCERTO_DDR_SHARED_SIZE) < 0)
@@ -26,4 +31,8 @@ void __init c2k_reserve(void)
 
 	if (memblock_remove(COMCERTO_DDR_SHARED_BASE, COMCERTO_DDR_SHARED_SIZE) < 0)
 		BUG();
+}
+
+phys_addr_t get_antirebootloop_ptr(void) {
+	return COMCERTO_AXI_DDR_BASE + (1 * PAGE_SIZE);
 }
