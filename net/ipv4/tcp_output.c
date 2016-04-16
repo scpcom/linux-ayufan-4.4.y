54e813aadc282ac6ce94748a9a4a31fe875aa55f
Avery Pennarun <apenwarr@gmail.com>
2016-04-16 04:15:07 -0400

    Set TCP small queues to ~16ms instead of ~1ms.
    This allows us to enqueue at least a few wifi or moca aggregates.  Otherwise
    we can't saturate a link.
    
    Change-Id: I3129fc44a5ae28e5a41f5f9b571117200ef568de

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2103,7 +2103,7 @@ static bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 			break;
 
 		/* TCP Small Queues :
-		 * Control number of packets in qdisc/devices to two packets / or ~1 ms.
+		 * Control number of packets in qdisc/devices to two packets / or ~16 ms.
 		 * This allows for :
 		 *  - better RTT estimation and ACK scheduling
 		 *  - faster recovery
@@ -2112,7 +2112,7 @@ static bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 		 * of queued bytes to ensure line rate.
 		 * One example is wifi aggregation (802.11 AMPDU)
 		 */
-		limit = max(2 * skb->truesize, sk->sk_pacing_rate >> 10);
+		limit = max(2 * skb->truesize, sk->sk_pacing_rate >> 6);
 		limit = min_t(u32, limit, sysctl_tcp_limit_output_bytes);
 
 		if (atomic_read(&sk->sk_wmem_alloc) > limit) {
