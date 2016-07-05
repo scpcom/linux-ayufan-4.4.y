2badde653f002f6950ecf786a12987226d0e293e
Vegard Nossum <vegard.nossum@oracle.com>
2016-07-05 10:18:08 +0200

    xfrm: fix crash in XFRM_MSG_GETSA netlink handler
    If we hit any of the error conditions inside xfrm_dump_sa(), then
    xfrm_state_walk_init() never gets called. However, we still call
    xfrm_state_walk_done() from xfrm_dump_sa_done(), which will crash
    because the state walk was never initialized properly.
    
    We can fix this by setting cb->args[0] only after we've processed the
    first element and checking this before calling xfrm_state_walk_done().
    
    Fixes: d3623099d3 ("ipsec: add support of limited SA dump")
    Cc: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Acked-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    [bwh: Backported to 3.2: There is no filtering in xfrm_dump_sa(), but this crash
     does seem to be possible if no SA is present at all.]

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -855,7 +855,8 @@ nla_put_failure:
 static int xfrm_dump_sa_done(struct netlink_callback *cb)
 {
 	struct xfrm_state_walk *walk = (struct xfrm_state_walk *) &cb->args[1];
-	xfrm_state_walk_done(walk);
+	if (cb->args[0])
+		xfrm_state_walk_done(walk);
 	return 0;
 }
 
