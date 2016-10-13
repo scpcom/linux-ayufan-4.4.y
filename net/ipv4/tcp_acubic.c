80d9f311195aa2cf12f084bb54c46ab0895d8afa
Avery Pennarun <apenwarr@gmail.com>
2016-10-12 21:04:48 -0400

    Oops, tcp_acubic.c had leftover references to its old name, laminar.
    Change-Id: I2f0b10d1dc9c63817c95cc95b5cd870496d95d2f

--- net/ipv4/tcp_acubic.c
+++ net/ipv4/tcp_acubic.c
@@ -1,5 +1,5 @@
 /*
- * TCP Laminar: a variation on CUBIC but with a more aggressive RTO.
+ * TCP ACubic: a variation on CUBIC but with a more aggressive RTO.
  * You shouldn't do this on the open Internet; it could cause congestion
  * collapse problems.  It might be okay in a more controlled environment.
  */
@@ -11,16 +11,16 @@
 
 static void (*old_cubic_init)(struct sock *sk);
 
-static void laminar_init(struct sock *sk)
+static void acubic_init(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	old_cubic_init(sk);
 	tp->is_aggressive_rto = 1;
 }
 
-static struct tcp_congestion_ops laminartcp __read_mostly;
+static struct tcp_congestion_ops acubictcp __read_mostly;
 
-static int __init laminar_register(void)
+static int __init acubic_register(void)
 {
 	struct tcp_congestion_ops *ca;
 	u32 key = tcp_ca_get_key_by_name("cubic");
@@ -33,23 +33,23 @@ static int __init laminar_register(void)
 	// Wrap CUBIC's implementation, but replace the name and init
 	// function.  This way we can measure performance differences by
 	// tracking just the name of the congestion algorithm.
-	memcpy(&laminartcp, ca, sizeof(struct tcp_congestion_ops));
-	strncpy(laminartcp.name, "laminar", sizeof(laminartcp.name));
+	memcpy(&acubictcp, ca, sizeof(struct tcp_congestion_ops));
+	strncpy(acubictcp.name, "acubic", sizeof(acubictcp.name));
 	old_cubic_init = ca->init;
-	laminartcp.owner = THIS_MODULE;
-	laminartcp.init = laminar_init;
-	return tcp_register_congestion_control(&laminartcp);
+	acubictcp.owner = THIS_MODULE;
+	acubictcp.init = acubic_init;
+	return tcp_register_congestion_control(&acubictcp);
 }
 
-static void __exit laminar_unregister(void)
+static void __exit acubic_unregister(void)
 {
-	tcp_unregister_congestion_control(&laminartcp);
+	tcp_unregister_congestion_control(&acubictcp);
 }
 
-module_init(laminar_register);
-module_exit(laminar_unregister);
+module_init(acubic_register);
+module_exit(acubic_unregister);
 
 MODULE_AUTHOR("Avery Pennarun");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Laminar TCP");
+MODULE_DESCRIPTION("ACubic TCP");
 MODULE_VERSION("1.0");
