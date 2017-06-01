269b2b905b66092bccd354b1316df13ae0490931
Liang Chen <cl@rock-chips.com>
2017-06-01 11:11:11 +0800

    soc: rockchip: cpuinfo: read cpu version from eFuse
    Change-Id: Ia18ff4e745f09fa04690bb7bc6d95169c389b9d2
    Signed-off-by: Liang Chen <cl@rock-chips.com>

--- drivers/soc/rockchip/rockchip-cpuinfo.c
+++ drivers/soc/rockchip/rockchip-cpuinfo.c
@@ -19,6 +19,10 @@
 #include <linux/of_platform.h>
 #include <linux/slab.h>
 #include <asm/system_info.h>
+#include <linux/rockchip/cpu.h>
+
+unsigned long rockchip_soc_id;
+EXPORT_SYMBOL(rockchip_soc_id);
 
 static int rockchip_cpuinfo_probe(struct platform_device *pdev)
 {
@@ -27,6 +31,16 @@ static int rockchip_cpuinfo_probe(struct platform_device *pdev)
 	unsigned char *efuse_buf, buf[16];
 	size_t len;
 	int i;
+
+	cell = nvmem_cell_get(dev, "cpu-version");
+	if (!IS_ERR(cell)) {
+		efuse_buf = nvmem_cell_read(cell, &len);
+		nvmem_cell_put(cell);
+
+		if (len == 1)
+			rockchip_set_cpu_version(efuse_buf[0]);
+		kfree(efuse_buf);
+	}
 
 	cell = nvmem_cell_get(dev, "id");
 	if (IS_ERR(cell)) {
@@ -73,7 +87,12 @@ static struct platform_driver rockchip_cpuinfo_driver = {
 		.of_match_table = rockchip_cpuinfo_of_match,
 	},
 };
-module_platform_driver(rockchip_cpuinfo_driver);
+
+static int __init rockchip_cpuinfo_init(void)
+{
+	return platform_driver_register(&rockchip_cpuinfo_driver);
+}
+subsys_initcall_sync(rockchip_cpuinfo_init);
 
 MODULE_DESCRIPTION("Rockchip CPU info driver");
 MODULE_AUTHOR("Huang, Tao <huangtao@rock-chips.com>");
