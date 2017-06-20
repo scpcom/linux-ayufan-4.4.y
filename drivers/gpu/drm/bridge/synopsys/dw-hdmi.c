e7e66e5151cf66ebfec603f6e12afffe9ff4199f
Huicong Xu <xhc@rock-chips.com>
2017-06-20 11:40:43 +0800

    drm: bridge: dw-hdmi: set hdcp1x_enable in the dts so uboot can visit
    Change-Id: Ibaf91c5beac2355e5c270f1edb69a63795dbff6a
    Signed-off-by: Huicong Xu <xhc@rock-chips.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -4147,7 +4147,7 @@ static void dw_hdmi_register_debugfs(struct device *dev, struct dw_hdmi *hdmi)
 }
 
 static void dw_hdmi_register_hdcp(struct device *dev, struct dw_hdmi *hdmi,
-				  u32 val)
+				  u32 val, bool hdcp1x_enable)
 {
 	struct dw_hdcp hdmi_hdcp = {
 		.hdmi = hdmi,
@@ -4155,7 +4155,7 @@ static void dw_hdmi_register_hdcp(struct device *dev, struct dw_hdmi *hdmi,
 		.read = hdmi_readb,
 		.regs = hdmi->regs,
 		.reg_io_width = val,
-		.enable = 0,
+		.enable = hdcp1x_enable,
 	};
 	struct platform_device_info hdcp_device_info = {
 		.parent = dev,
@@ -4241,6 +4241,7 @@ struct dw_hdmi *dw_hdmi_probe(struct platform_device *pdev,
 	u8 prod_id1;
 	u8 config0;
 	u8 config3;
+	bool hdcp1x_enable = 0;
 
 	hdmi = devm_kzalloc(dev, sizeof(*hdmi), GFP_KERNEL);
 	if (!hdmi)
@@ -4573,7 +4574,10 @@ struct dw_hdmi *dw_hdmi_probe(struct platform_device *pdev,
 	drm_bridge_add(&hdmi->bridge);
 
 	dw_hdmi_register_debugfs(dev, hdmi);
-	dw_hdmi_register_hdcp(dev, hdmi, val);
+
+	if (of_property_read_bool(np, "hdcp1x-enable"))
+		hdcp1x_enable = 1;
+	dw_hdmi_register_hdcp(dev, hdmi, val, hdcp1x_enable);
 
 	return hdmi;
 
