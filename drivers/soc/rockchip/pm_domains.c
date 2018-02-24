b285d2556096f510d63b4528c19e347bc23e5106
Finley Xiao <finley.xiao@rock-chips.com>
2018-02-24 16:57:57 +0800

    soc: rockchip: power-domain: Fix restore error qos value
    Change-Id: I74692018652ed2aa45b666f1598662146beec92e
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/soc/rockchip/pm_domains.c
+++ drivers/soc/rockchip/pm_domains.c
@@ -83,6 +83,7 @@ struct rockchip_pm_domain {
 	int num_clks;
 	struct clk_bulk_data *clks;
 	bool is_ignore_pwr;
+	bool is_qos_saved;
 };
 
 struct rockchip_pmu {
@@ -524,6 +525,7 @@ static int rockchip_pd_power(struct rockchip_pm_domain *pd, bool power_on)
 
 		if (!power_on) {
 			rockchip_pmu_save_qos(pd);
+			pd->is_qos_saved = true;
 
 			/* if powering down, idle request to NIU first */
 			rockchip_pmu_set_idle_request(pd, true);
@@ -535,7 +537,8 @@ static int rockchip_pd_power(struct rockchip_pm_domain *pd, bool power_on)
 			/* if powering up, leave idle mode */
 			rockchip_pmu_set_idle_request(pd, false);
 
-			rockchip_pmu_restore_qos(pd);
+			if (pd->is_qos_saved)
+				rockchip_pmu_restore_qos(pd);
 		}
 
 		clk_bulk_disable(pd->num_clks, pd->clks);
