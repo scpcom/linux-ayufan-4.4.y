eddace7dd41a4cea14e5832b51a88181b1236cfc
Algea Cao <algea.cao@rock-chips.com>
2019-01-03 10:50:33 +0800

    drm: bridge: dw-hdmi: Check whether hdmi is initialized when hdmi suspend or resume
    Hdmi suspend or resume may be called before hdmi initialization. We must
    verify that hdmi is initialized first.
    
    Change-Id: I2a680209e64b9c1aebc2d9ee19d543927137afd0
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -4611,6 +4611,9 @@ static void dw_hdmi_reg_initial(struct dw_hdmi *hdmi)
 
 void dw_hdmi_suspend(struct dw_hdmi *hdmi)
 {
+	if (!hdmi)
+		return;
+
 	mutex_lock(&hdmi->mutex);
 
 	/*
@@ -4636,6 +4639,9 @@ EXPORT_SYMBOL_GPL(dw_hdmi_suspend);
 
 void dw_hdmi_resume(struct dw_hdmi *hdmi)
 {
+	if (!hdmi)
+		return;
+
 	pinctrl_pm_select_default_state(hdmi->dev);
 	mutex_lock(&hdmi->mutex);
 	dw_hdmi_reg_initial(hdmi);
