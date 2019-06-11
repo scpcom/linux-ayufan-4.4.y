51eb2f375a675c316be9f5b6e8fa1ef86179ab38
Sandy Huang <hjc@rock-chips.com>
2019-06-11 16:29:48 +0800

    drm/rockchip: gem: don't limit to 32bit mapping when not support LPAE
    Change-Id: Ia4fab3d63947ba693488fb58e3a104d400bd6e23
    Signed-off-by: Mark Yao <mark.yao@rock-chips.com>
    Signed-off-by: Jianqun Xu <jay.xu@rock-chips.com>
    Signed-off-by: Meiyou Chen <cmy@rock-chips.com>
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_gem.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_gem.c
@@ -575,9 +575,15 @@ static const struct drm_gem_object_funcs rockchip_gem_object_funcs = {
 static struct rockchip_gem_object *
 	rockchip_gem_alloc_object(struct drm_device *drm, unsigned int size)
 {
+	struct address_space *mapping;
 	struct rockchip_gem_object *rk_obj;
 	struct drm_gem_object *obj;
 
+#ifdef CONFIG_ARM_LPAE
+	gfp_t gfp_mask = GFP_HIGHUSER | __GFP_RECLAIMABLE | __GFP_DMA32;
+#else
+	gfp_t gfp_mask = GFP_HIGHUSER | __GFP_RECLAIMABLE;
+#endif
 	size = round_up(size, PAGE_SIZE);
 
 	rk_obj = kzalloc(sizeof(*rk_obj), GFP_KERNEL);
@@ -589,6 +595,9 @@ static struct rockchip_gem_object *
 	obj->funcs = &rockchip_gem_object_funcs;
 
 	drm_gem_object_init(drm, obj, size);
+
+	mapping = file_inode(obj->filp)->i_mapping;
+	mapping_set_gfp_mask(mapping, gfp_mask);
 
 	return rk_obj;
 }
