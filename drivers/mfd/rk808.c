496c00dcdfae616fd6c8d6d9f54a2bfc66c66655
Seungchul Lee <john.lee@hardkernel.com>
2019-07-16 11:11:49 +0900

    mfd: rk808: Fix broken poweroff and reboot of rk817
    Change-Id: Ia0f7b4c92acd62139d6133e156c55a4fa960714f

--- drivers/mfd/rk808.c
+++ drivers/mfd/rk808.c
@@ -1117,7 +1117,9 @@ static int rk817_reboot_notifier_handler(struct notifier_block *nb,
 {
 	struct rk817_reboot_data_t *data;
 	struct device *dev;
+#ifndef CONFIG_ARCH_ROCKCHIP_ODROIDGO2
 	int value, power_en_active0, power_en_active1;
+#endif
 	int ret, i;
 	static const char * const pmic_rst_reg_only_cmd[] = {
 		"loader", "bootloader", "fastboot", "recovery",
@@ -1127,6 +1129,7 @@ static int rk817_reboot_notifier_handler(struct notifier_block *nb,
 	data = container_of(nb, struct rk817_reboot_data_t, reboot_notifier);
 	dev = &data->rk808->i2c->dev;
 
+#ifndef CONFIG_ARCH_ROCKCHIP_ODROIDGO2
 	regmap_read(data->rk808->regmap, RK817_POWER_EN_SAVE0,
 		    &power_en_active0);
 	if (power_en_active0 != 0) {
@@ -1151,6 +1154,7 @@ static int rk817_reboot_notifier_handler(struct notifier_block *nb,
 	} else {
 		dev_info(dev, "reboot: not restore POWER_EN\n");
 	}
+#endif
 
 	if (action != SYS_RESTART || !cmd)
 		return NOTIFY_OK;
