c866bf2f2919aaee30b9dc6ee6d2ead7f0c4f3aa
charles.park <charles.park@hardkernel.com>
2019-07-25 18:38:23 +0900

    ODROID-GO2 : dsi panel data update & panel driver modified.
    Change-Id: Idcdbd9b21b0b3f131711fc342e4fdfbbc55ecfe0

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -136,6 +136,9 @@ struct panel_simple {
 
 	const struct panel_desc *desc;
 
+#if defined(CONFIG_ARCH_ROCKCHIP_ODROIDGO2)
+	struct regulator *backlight_supply;
+#endif
 	struct regulator *supply;
 	struct i2c_adapter *ddc;
 
@@ -397,8 +400,14 @@ static int panel_simple_regulator_enable(struct panel_simple *p)
 		if (err < 0)
 			return err;
 	}
-
+#if defined(CONFIG_ARCH_ROCKCHIP_ODROIDGO2)
+	err = regulator_enable(p->backlight_supply);
+	if (err < 0)
+		dev_err(p->base.dev, "failed to enable supply: %d\n", err);
+	return err;
+#else
 	return 0;
+#endif
 }
 
 static int panel_simple_regulator_disable(struct panel_simple *p)
@@ -415,7 +424,12 @@ static int panel_simple_regulator_disable(struct panel_simple *p)
 		regulator_disable(p->supply);
 	}
 
+#if defined(CONFIG_ARCH_ROCKCHIP_ODROIDGO2)
+	regulator_disable(p->backlight_supply);
+	return err;
+#else
 	return 0;
+#endif
 }
 
 int panel_simple_loader_protect(struct drm_panel *panel)
@@ -462,7 +476,11 @@ static int panel_simple_unprepare(struct drm_panel *panel)
 		if (p->dsi)
 			panel_simple_xfer_dsi_cmd_seq(p, p->desc->exit_seq);
 
+#if defined(CONFIG_ARCH_ROCKCHIP_ODROIDGO2)
+	gpiod_direction_output(p->reset_gpio, 0);
+#else
 	gpiod_direction_output(p->reset_gpio, 1);
+#endif
 	gpiod_direction_output(p->enable_gpio, 0);
 
 	panel_simple_regulator_disable(p);
@@ -544,12 +562,20 @@ static int panel_simple_prepare(struct drm_panel *panel)
 		}
 	}
 
+#if defined(CONFIG_ARCH_ROCKCHIP_ODROIDGO2)
+	gpiod_direction_output(p->reset_gpio, 0);
+#else
 	gpiod_direction_output(p->reset_gpio, 1);
+#endif
 
 	if (p->desc->delay.reset)
 		msleep(p->desc->delay.reset);
 
+#if defined(CONFIG_ARCH_ROCKCHIP_ODROIDGO2)
+	gpiod_direction_output(p->reset_gpio, 1);
+#else
 	gpiod_direction_output(p->reset_gpio, 0);
+#endif
 
 	if (p->desc->delay.init)
 		msleep(p->desc->delay.init);
@@ -802,6 +828,11 @@ static int panel_simple_probe(struct device *dev, const struct panel_desc *desc)
 		return err;
 	}
 
+#if defined(CONFIG_ARCH_ROCKCHIP_ODROIDGO2)
+	panel->backlight_supply = devm_regulator_get(dev, "backlight");
+	if (IS_ERR(panel->backlight_supply))
+		return PTR_ERR(panel->backlight_supply);
+#endif
 	panel->enable_gpio = devm_gpiod_get_optional(dev, "enable", GPIOD_ASIS);
 	if (IS_ERR(panel->enable_gpio)) {
 		err = PTR_ERR(panel->enable_gpio);
