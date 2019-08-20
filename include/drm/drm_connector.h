0b58ea106eed5e33a9fd13fb7aaba75a097a7863
Algea Cao <algea.cao@rock-chips.com>
2019-08-20 09:32:06 +0800

    FROMLIST: drm: Parse Colorimetry data block from EDID
    CEA 861.3 spec adds colorimetry data block for HDMI.
    Parsing the block to get the colorimetry data from
    panel.
    
    v2: Rebase
    
    v3: No Change
    
    v4: Addressed Shashank's review comments. Updated
    colorimetry field to 16 bit as DCI-P3 got added
    in CEA 861-G spec, as pointed out by Shashank.
    
    v5: Fixed checkpatch warnings with --strict option.
    
    Change-Id: Ia82d4c04edff53bd4d6c4411dd90391497140e85
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Reviewed-by: Shashank Sharma <shashank.sharma@intel.com>
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>
    (am from https://patchwork.kernel.org/patch/10861325/)
    
    rk-5.4 f98d9dfaa8c9e7a41ec609e9328c012359e3676a

--- include/drm/drm_connector.h
+++ include/drm/drm_connector.h
@@ -207,6 +207,9 @@ struct drm_hdmi_info {
 
 	/** @y420_dc_modes: bitmap of deep color support index */
 	u8 y420_dc_modes;
+
+	/* @colorimetry: bitmap of supported colorimetry modes */
+	u16 colorimetry;
 };
 
 /**
