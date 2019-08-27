a504f17c8ed68d1ba4430950d920a566e928f1ea
Sugar Zhang <sugar.zhang@rock-chips.com>
2019-08-27 21:16:41 +0800

    ASoC: codec: hdmi-codec: add support for audio mode config
    Change-Id: I4813e6204fee894ef4f40b3e3b768a1ad94d0a29
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
    
    rk-5.4 d4c7ab7decfb68c03cd9c08af70556e8926b5a95

--- include/sound/hdmi-codec.h
+++ include/sound/hdmi-codec.h
@@ -45,6 +45,13 @@ struct hdmi_codec_params {
 	int sample_rate;
 	int sample_width;
 	int channels;
+	int mode;
+};
+
+enum {
+	LPCM = 0,
+	NLPCM,
+	HBR,
 };
 
 typedef void (*hdmi_codec_plugged_cb)(struct device *dev,
