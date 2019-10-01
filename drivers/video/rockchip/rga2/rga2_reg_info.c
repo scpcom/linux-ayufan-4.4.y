2e97f321e59686cab670e2b7439a55b4cdecbc25
OtherCrashOverride <OtherCrashOverride@users.noreply.github.com>
2019-10-01 07:25:44 -0500

    ODROID-GO2: RGA2: Pass through scale_mode when converting from RGA1.
    Change-Id: I17a2d8935bd3643172a264bf7e2b321f4689e752
    (cherry picked from commit 8277880dba81321326c655b404d4164f7bd59d0d)

--- drivers/video/rockchip/rga2/rga2_reg_info.c
+++ drivers/video/rockchip/rga2/rga2_reg_info.c
@@ -1256,6 +1256,7 @@ void RGA_MSG_2_RGA2_MSG(struct rga_req *req_rga, struct rga2_req *req)
         break;
     }
 
+    req->scale_bicu_mode = req_rga->scale_mode;
     if((req->dst.act_w > 2048) && (req->src.act_h < req->dst.act_h))
         req->scale_bicu_mode |= (1<<4);
 
@@ -1527,6 +1528,7 @@ void RGA_MSG_2_RGA2_MSG_32(struct rga_req_32 *req_rga, struct rga2_req *req)
         break;
     }
 
+    req->scale_bicu_mode = req_rga->scale_mode;
     if((req->dst.act_w > 2048) && (req->src.act_h < req->dst.act_h))
         req->scale_bicu_mode |= (1<<4);
     req->LUT_addr = req_rga->LUT_addr;
