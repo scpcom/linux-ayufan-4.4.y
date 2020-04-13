2b68509c7d64224dda809f355c67df7f4e92c057
scpcom <scpcom@gmx.de>
2020-04-13 06:56:55 +0200

    mainline rockchip-usb-serial-temp_re-add_lte_4g

--- drivers/usb/serial/usb_wwan.c
+++ drivers/usb/serial/usb_wwan.c
@@ -497,6 +497,7 @@ static struct urb *usb_wwan_setup_urb(struct usb_serial_port *port,
 	struct usb_serial *serial = port->serial;
 	struct usb_wwan_intf_private *intfdata = usb_get_serial_data(serial);
 	struct urb *urb;
+	struct usb_device_descriptor *desc = &serial->dev->descriptor;
 
 	urb = usb_alloc_urb(0, GFP_KERNEL);	/* No ISO */
 	if (!urb)
@@ -508,6 +509,12 @@ static struct urb *usb_wwan_setup_urb(struct usb_serial_port *port,
 
 	if (intfdata->use_zlp && dir == USB_DIR_OUT)
 		urb->transfer_flags |= URB_ZERO_PACKET;
+
+	if (dir == USB_DIR_OUT) {
+		if ((desc->idVendor == cpu_to_le16(0x1286) &&
+		     desc->idProduct == cpu_to_le16(0x4e3c)))
+			urb->transfer_flags |= URB_ZERO_PACKET;
+	}
 
 	return urb;
 }
