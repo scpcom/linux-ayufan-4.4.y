e9a644878898d3154cd8563ab40df0ebd67d0899
Joy Cho <joy.cho@hardkernel.com>
2020-05-05 14:50:41 +0900

    bluetooth: qca: to setup flexible firmware
    qca9377a-3-bt (hci_qca.patch from agency)
    
    Chip: QCA9377-3
    Module: Fn-Link 8223x-SR
    Change-Id: Id940875942b715f927ec42ff912b89fcc9929a9a

--- drivers/bluetooth/hci_qca.c
+++ drivers/bluetooth/hci_qca.c
@@ -618,6 +618,7 @@ static int qca_open(struct hci_uart *hu)
 	return 0;
 }
 
+#ifndef CONFIG_ARCH_ROCKCHIP_ODROIDGO2
 static void qca_debugfs_init(struct hci_dev *hdev)
 {
 	struct hci_uart *hu = hci_get_drvdata(hdev);
@@ -663,6 +664,7 @@ static void qca_debugfs_init(struct hci_dev *hdev)
 	debugfs_create_u32("tx_idle_delay", mode, ibs_dir,
 			   &qca->tx_idle_delay);
 }
+#endif
 
 /* Flush protocol data */
 static int qca_flush(struct hci_uart *hu)
@@ -1706,6 +1708,7 @@ retry:
 	}
 
 	bt_dev_info(hdev, "QCA controller version 0x%08x", soc_ver);
+#ifndef CONFIG_ARCH_ROCKCHIP_ODROIDGO2
 	/* Setup patch / NVM configurations */
 	ret = qca_uart_setup(hdev, qca_baudrate, soc_type, soc_ver,
 			firmware_name);
@@ -1738,6 +1741,7 @@ retry:
 			goto retry;
 		}
 	}
+#endif
 
 	/* Setup bdaddr */
 	if (soc_type == QCA_ROME)
@@ -1753,7 +1757,11 @@ static const struct hci_uart_proto qca_proto = {
 	.name		= "QCA",
 	.manufacturer	= 29,
 	.init_speed	= 115200,
+#ifndef CONFIG_ARCH_ROCKCHIP_ODROIDGO2
 	.oper_speed	= 3000000,
+#else
+	.oper_speed	= 2000000,
+#endif
 	.open		= qca_open,
 	.close		= qca_close,
 	.flush		= qca_flush,
