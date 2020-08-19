3c2aca9ce69b456071813f6dbb8eb6c108c2b221
scpcom <scpcom@gmx.de>
2020-08-19 02:40:46 +0200

    drm/rockchip: dw_hdmi: Re-add use_drm_infoframe flag
    rk-5.4 a3ae5091f8c4fc8a271f6a7943df730cda3976df

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -1217,6 +1217,7 @@ static const struct dw_hdmi_plat_data rk3328_hdmi_drv_data = {
 	.phy_ops = &rk3328_hdmi_phy_ops,
 	.phy_name = "inno_dw_hdmi_phy2",
 	.phy_force_vendor = true,
+	.use_drm_infoframe = true,
 	.ycbcr_420_allowed = true,
 };
 
@@ -1247,6 +1248,7 @@ static const struct dw_hdmi_plat_data rk3399_hdmi_drv_data = {
 	.cur_ctr    = rockchip_cur_ctr,
 	.phy_config = rockchip_phy_config,
 	.phy_data = &rk3399_chip_data,
+	.use_drm_infoframe = true,
 	.ycbcr_420_allowed = true,
 };
 
