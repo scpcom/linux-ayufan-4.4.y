82f7e95f9d8c5350389ec1ae3a7355ec5024fbf8
Sandy Huang <hjc@rock-chips.com>
2021-03-04 16:41:51 +0800

    drm/rockchp: drv: Add support for more than 4G memory
    fix log:
    
    rockchip-drm display-subsystem: swiotlb buffer is full (sz: 262144 bytes)
    
    Change-Id: I05fd8a2674e1e73fb1d35c75c009eacc7ba8a236
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.c
@@ -1134,6 +1134,7 @@ static int rockchip_drm_platform_probe(struct platform_device *pdev)
 		rockchip_drm_match_remove(dev);
 		return ret;
 	}
+	dev->coherent_dma_mask = DMA_BIT_MASK(64);
 
 	return 0;
 }
