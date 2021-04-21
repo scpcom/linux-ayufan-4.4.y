1fc3adb3ca610d68d24a1622907b773e502215fd
Elaine Zhang <zhangqing@rock-chips.com>
2021-04-21 18:05:38 +0800

    mfd: rk808: close rtc int when power off
    Change-Id: I1f1bfe3d6c106632c45b51bec3c18361572df865
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/mfd/rk8xx-core.c
+++ drivers/mfd/rk8xx-core.c
@@ -803,6 +803,20 @@ void rk8xx_shutdown(struct device *dev)
 	struct rk808 *rk808 = dev_get_drvdata(dev);
 	int ret = 0;
 
+	if (!rk808) {
+		dev_warn(dev,
+			 "have no rk808, so do nothing here\n");
+		return;
+	}
+
+	/* close rtc int when power off */
+	regmap_update_bits(rk808->regmap,
+			   RK808_INT_STS_MSK_REG1,
+			   (0x3 << 5), (0x3 << 5));
+	regmap_update_bits(rk808->regmap,
+			   RK808_RTC_INT_REG,
+			   (0x3 << 2), (0x0 << 2));
+
 	switch (rk808->variant) {
 	case RK805_ID:
 		ret = regmap_update_bits(rk808->regmap,
