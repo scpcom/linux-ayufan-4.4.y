48355cb04dfbe0695a99a7391ac4e20bc65cac6e
Andy Yan <andy.yan@rock-chips.com>
2021-06-29 10:04:11 +0800

    drm/rockchip: Add rockchip_drm_of_find_possible_crtcs() function
    When get possible_crtcs for a encoder, skip the inactived endpoint, this
    make userspace more clear about which crtc this connector should attached
    to.
    
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>
    Change-Id: I4221491307193209482fd9477a492ebba613c7ad

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.h
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.h
@@ -187,6 +187,9 @@ void rockchip_unregister_crtc_funcs(struct drm_crtc *crtc);
 int rockchip_drm_encoder_set_crtc_endpoint_id(struct rockchip_encoder *rencoder,
 					      struct device_node *np, int port, int reg);
 int rockchip_drm_endpoint_is_subdriver(struct device_node *ep);
+uint32_t rockchip_drm_of_find_possible_crtcs(struct drm_device *dev,
+					     struct device_node *port);
+
 extern struct platform_driver cdn_dp_driver;
 extern struct platform_driver dw_hdmi_rockchip_pltfm_driver;
 extern struct platform_driver dw_mipi_dsi_rockchip_driver;
