22110f935c5894a37fd0811e68b7942d23a25380
Algea Cao <algea.cao@rock-chips.com>
2021-08-04 17:05:20 +0800

    drm/rockchip: dw-hdmi: init hdmi subdev and register to rockchip_drm_sub_dev_list
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>
    Change-Id: I91fe4412c1218b266d1cafc8dc92781dcaf23638

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -127,6 +127,7 @@ struct rockchip_hdmi {
 	unsigned int hdmi_quant_range;
 	unsigned int phy_bus_width;
 	enum drm_hdmi_output_type hdmi_output;
+	struct rockchip_drm_sub_dev sub_dev;
 };
 
 static struct rockchip_hdmi *to_rockchip_hdmi(struct drm_encoder *encoder)
@@ -1310,6 +1311,10 @@ static int dw_hdmi_rockchip_bind(struct device *dev, struct device *master,
 		goto err_bind;
 	}
 
+	hdmi->sub_dev.connector = plat_data->connector;
+	hdmi->sub_dev.of_node = dev->of_node;
+	rockchip_drm_register_sub_dev(&hdmi->sub_dev);
+
 	return 0;
 
 err_bind:
@@ -1329,6 +1334,7 @@ static void dw_hdmi_rockchip_unbind(struct device *dev, struct device *master,
 {
 	struct rockchip_hdmi *hdmi = dev_get_drvdata(dev);
 
+	rockchip_drm_unregister_sub_dev(&hdmi->sub_dev);
 	dw_hdmi_unbind(hdmi->hdmi);
 	clk_disable_unprepare(hdmi->phyref_clk);
 	clk_disable_unprepare(hdmi->hclk_vop);
