b0de24e9d6ab960ba49271db9f9a4544569f2b9e
scpcom <scpcom@gmx.de>
2021-08-27 18:22:46 +0200

    3776-to-4418: add/remove files

---
+++ include/linux/decompress/unlzo_mm.h
@@ -0,0 +1,15 @@
+#ifndef MY_ABC_HERE
+#define MY_ABC_HERE
+#endif
+#ifndef UNLZO_MM_H
+#define UNLZO_MM_H
+
+#if defined(CONFIG_SYNO_COMCERTO)
+#ifdef STATIC
+#define INIT
+#else
+#define INIT __init
+#endif
+#endif
+
+#endif
