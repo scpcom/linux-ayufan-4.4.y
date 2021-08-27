f84bb6e044b7bbda080274958aa14179bdf7e48d
scpcom <scpcom@gmx.de>
2021-08-27 18:20:20 +0200

    mainline-to-2636: add/remove files

---
+++ include/linux/syno_debug.h
@@ -0,0 +1,14 @@
+#ifndef MY_ABC_HERE
+#define MY_ABC_HERE
+#endif
+/* Copyright (c) 2008 Synology Inc. All rights reserved. */
+ 
+#ifndef __SYNODEBUG_H_
+#define __SYNODEBUG_H_
+
+/**
+ * IN : sd.c, scsi.h, scsi_lib.c
+ */
+#define SYNO_BADSECTOR_TEST
+
+#endif /* __SYNODEBUG_H_ */
