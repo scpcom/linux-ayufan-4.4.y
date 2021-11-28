25d8a191feb0db303d12b9b1bc4141286427b560
scpcom <scpcom@gmx.de>
2021-11-28 21:23:25 +0100

    smartx-d1-tina-v1.0.0-release: sunxi-iommu changes

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1984,8 +1984,12 @@ int iommu_attach_device(struct iommu_domain *domain, struct device *dev)
 	 */
 	mutex_lock(&group->mutex);
 	ret = -EINVAL;
-	if (iommu_group_device_count(group) != 1)
+	if (iommu_group_device_count(group) != 1) {
+#ifdef CONFIG_ARCH_SUNXI
+		ret =  __iommu_attach_device(domain, dev);
+#endif
 		goto out_unlock;
+	}
 
 	ret = __iommu_attach_group(domain, group);
 
@@ -2454,7 +2458,11 @@ static ssize_t __iommu_map_sg(struct iommu_domain *domain, unsigned long iova,
 	phys_addr_t start;
 	unsigned int i = 0;
 	int ret;
+#ifdef CONFIG_ARCH_SUNXI
+	struct iommu_iotlb_gather iotlb_gather;
 
+	prot |= (1 << 16);
+#endif
 	while (i <= nents) {
 		phys_addr_t s_phys = sg_phys(sg);
 
@@ -2486,6 +2494,12 @@ next:
 
 	if (ops->iotlb_sync_map)
 		ops->iotlb_sync_map(domain, iova, mapped);
+#ifdef CONFIG_ARCH_SUNXI
+	iotlb_gather.start = iova;
+	iotlb_gather.end = iova + mapped;
+	if (domain->ops->iotlb_sync)
+		domain->ops->iotlb_sync(domain, &iotlb_gather);
+#endif
 
 	return mapped;
 
