4bf04ee48ccb59bcff63f54b47644324353e2583
scpcom <scpcom@gmx.de>
2021-12-04 03:11:20 +0100

    xr829: cleanup internal header copies

--- include/linux/ieee80211.h
+++ include/linux/ieee80211.h
@@ -1320,6 +1320,13 @@ struct ieee80211_mgmt {
 				} __packed chan_switch;
 				struct{
 					u8 action_code;
+					u8 element_id;
+					u8 length;
+					struct ieee80211_channel_sw_ie sw_elem;
+					u8 variable[];
+				} __packed swe_chan_switch;
+				struct{
+					u8 action_code;
 					struct ieee80211_ext_chansw_ie data;
 					u8 variable[];
 				} __packed ext_chan_switch;
@@ -3556,6 +3563,8 @@ enum ieee80211_eid {
 	WLAN_EID_RSNX = 244,
 	WLAN_EID_EXTENSION = 255
 };
+
+#define WLAN_EID_HT_INFORMATION WLAN_EID_HT_OPERATION
 
 /* Element ID Extensions for Element ID 255 */
 enum ieee80211_eid_ext {
