f2686828f742572715ca54d344a2aa5f88fb844e
scpcom <scpcom@gmx.de>
2021-12-04 03:11:20 +0100

    xr829: cleanup internal header copies

---
+++ drivers/net/wireless/xr829/include/linux/ieee80211_xr.h
@@ -0,0 +1,20 @@
+#ifndef IEEE80211_XR_H
+#define IEEE80211_XR_H
+
+/**
+ * compare_ether_addr - Compare two Ethernet addresses
+ * @addr1: Pointer to a six-byte array containing the Ethernet address
+ * @addr2: Pointer other six-byte array containing the Ethernet address
+ *
+ * Compare two Ethernet addresses, returns 0 if equal, non-zero otherwise.
+ * Unlike memcmp(), it doesn't return a value suitable for sorting.
+ */
+static inline unsigned compare_ether_addr(const u8 *addr1, const u8 *addr2)
+{
+	const u16 *a = (const u16 *) addr1;
+	const u16 *b = (const u16 *) addr2;
+
+	return ((a[0] ^ b[0]) | (a[1] ^ b[1]) | (a[2] ^ b[2])) != 0;
+}
+
+#endif
--- include/linux/ieee80211.h
+++ include/linux/ieee80211.h
@@ -974,6 +974,13 @@ struct ieee80211_mgmt {
 				} __packed chan_switch;
 				struct{
 					u8 action_code;
+					u8 element_id;
+					u8 length;
+					struct ieee80211_channel_sw_ie sw_elem;
+					u8 variable[0];
+				} __packed swe_chan_switch;
+				struct{
+					u8 action_code;
 					struct ieee80211_ext_chansw_ie data;
 					u8 variable[0];
 				} __packed ext_chan_switch;
@@ -2534,6 +2541,8 @@ enum ieee80211_eid {
 	WLAN_EID_FRAGMENT = 242,
 	WLAN_EID_EXTENSION = 255
 };
+
+#define WLAN_EID_HT_INFORMATION WLAN_EID_HT_OPERATION
 
 /* Element ID Extensions for Element ID 255 */
 enum ieee80211_eid_ext {
