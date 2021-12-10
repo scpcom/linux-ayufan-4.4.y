ee9ada845b3e541bd212633b6cc9fc75ee405c5d
scpcom <scpcom@gmx.de>
2021-12-10 02:11:47 +0100

    riscv: cpuinfo: Add Hardware, Revision and Serial under /proc/cpuinfo

--- arch/riscv/kernel/setup.c
+++ arch/riscv/kernel/setup.c
@@ -22,6 +22,7 @@
 #include <asm/pgtable.h>
 #include <asm/smp.h>
 #include <asm/sbi.h>
+#include <asm/system_info.h>
 #include <asm/tlbflush.h>
 #include <asm/thread_info.h>
 
@@ -42,10 +43,25 @@ struct screen_info screen_info = {
 atomic_t hart_lottery;
 unsigned long boot_cpu_hartid;
 
+static void __init setup_machine_fdt(void)
+{
+	const char *name;
+
+	name = of_flat_dt_get_machine_name();
+	if (!name)
+		return;
+	machine_name = name;
+
+	pr_info("Machine model: %s\n", name);
+	dump_stack_set_arch_desc("%s (DT)", name);
+}
+
 void __init parse_dtb(void)
 {
-	if (early_init_dt_scan(dtb_early_va))
+	if (early_init_dt_scan(dtb_early_va)) {
+		setup_machine_fdt();
 		return;
+	}
 
 	pr_err("No DTB passed to the kernel\n");
 #ifdef CONFIG_CMDLINE_FORCE
