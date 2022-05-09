ad90d4adc9d75c18c06388d666b25302bfd4a129
Wyon Bi <bivvy.bi@rock-chips.com>
2022-05-09 06:44:43 +0000

    drm/panel: simple: Add reset gpio
    Fixes: 12353d72438b ("drm/panel: simple: Add reset gpio")
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>
    Change-Id: I3c3066e4249aa49f948de9f0bb5d888f0e3d2739

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -564,7 +564,7 @@ static int panel_simple_resume(struct device *dev)
 
 	gpiod_direction_output(p->enable_gpio, 1);
 
-	if (p->desc->delay.reset)
+	if (p->desc->delay.prepare)
 		msleep(p->desc->delay.prepare);
 
 	gpiod_direction_output(p->reset_gpio, 1);
@@ -573,9 +573,6 @@ static int panel_simple_resume(struct device *dev)
 		msleep(p->desc->delay.reset);
 
 	gpiod_direction_output(p->reset_gpio, 0);
-
-	if (p->desc->delay.prepare)
-		msleep(p->desc->delay.prepare);
 
 	if (p->desc->init_seq)
 		if (p->dsi)
