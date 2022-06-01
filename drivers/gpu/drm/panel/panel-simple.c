5664353303ae3d1cfece1eff90c576434f48e638
Guochun Huang <hero.huang@rock-chips.com>
2022-06-01 10:38:08 +0800

    drm/panel: simple: delay init time after panel reset
    Signed-off-by: Guochun Huang <hero.huang@rock-chips.com>
    Change-Id: Ifb9b57bb7ce84263dea96958f6a4da9c7a6af545

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -659,12 +659,12 @@ static int panel_simple_prepare_once(struct panel_simple *p)
 
 	gpiod_direction_output(p->reset_gpio, 0);
 
+	if (p->desc->delay.init)
+		msleep(p->desc->delay.init);
+
 	if (p->desc->init_seq)
 		if (p->dsi)
 			panel_simple_xfer_dsi_cmd_seq(p, p->desc->init_seq);
-
-	if (p->desc->delay.init)
-		msleep(p->desc->delay.init);
 
 	p->prepared_time = ktime_get();
 
