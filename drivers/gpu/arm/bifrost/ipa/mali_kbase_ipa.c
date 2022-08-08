da3f86569f2088cf3d8da86efe379e1ad8014220
scpcom <scpcom@gmx.de>
2022-08-08 04:46:57 +0200

    arm: bifrost r12p0 fix build against 5.15

--- drivers/gpu/arm/bifrost/ipa/mali_kbase_ipa.c
+++ drivers/gpu/arm/bifrost/ipa/mali_kbase_ipa.c
@@ -482,6 +482,7 @@ static u32 get_static_power_locked(struct kbase_device *kbdev,
 	return power;
 }
 
+#if KERNEL_VERSION(5, 10, 0) > LINUX_VERSION_CODE
 #if defined(CONFIG_MALI_PWRSOFT_765) || \
 	LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
 static unsigned long kbase_get_static_power(struct devfreq *df,
@@ -513,7 +514,9 @@ static unsigned long kbase_get_static_power(unsigned long voltage)
 
 	return power;
 }
+#endif /* KERNEL_VERSION(5, 10, 0) > LINUX_VERSION_CODE */
 
+#if KERNEL_VERSION(5, 10, 0) > LINUX_VERSION_CODE
 #if defined(CONFIG_MALI_PWRSOFT_765) || \
 	LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
 static unsigned long kbase_get_dynamic_power(struct devfreq *df,
@@ -556,6 +559,7 @@ static unsigned long kbase_get_dynamic_power(unsigned long freq,
 
 	return power;
 }
+#endif /* KERNEL_VERSION(5, 10, 0) > LINUX_VERSION_CODE */
 
 int kbase_get_real_power_locked(struct kbase_device *kbdev, u32 *power,
 				unsigned long freq,
@@ -622,7 +626,9 @@ struct devfreq_cooling_ops kbase_ipa_power_model_ops = {
 #else
 struct devfreq_cooling_power kbase_ipa_power_model_ops = {
 #endif
+#if KERNEL_VERSION(5, 10, 0) > LINUX_VERSION_CODE
 	.get_static_power = &kbase_get_static_power,
 	.get_dynamic_power = &kbase_get_dynamic_power,
+#endif /* KERNEL_VERSION(5, 10, 0) > LINUX_VERSION_CODE */
 };
 KBASE_EXPORT_TEST_API(kbase_ipa_power_model_ops);
