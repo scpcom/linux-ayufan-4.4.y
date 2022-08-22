d6a669d73913763bc0ea088b1e6fd83f3d63e813
scpcom <scpcom@gmx.de>
2022-08-22 03:02:02 +0200

    touchscreen: gt9xxnew: use proc_ops

--- drivers/input/touchscreen/gt9xxnew/gt9xx.c
+++ drivers/input/touchscreen/gt9xxnew/gt9xx.c
@@ -94,10 +94,9 @@ static ssize_t gt91xx_config_read_proc(struct file *, char __user *, size_t, lof
 static ssize_t gt91xx_config_write_proc(struct file *, const char __user *, size_t, loff_t *);
 
 static struct proc_dir_entry *gt91xx_config_proc;
-static const struct file_operations config_proc_ops = {
-	.owner = THIS_MODULE,
-	.read = gt91xx_config_read_proc,
-	.write = gt91xx_config_write_proc,
+static const struct proc_ops config_proc_ops = {
+	.proc_read = gt91xx_config_read_proc,
+	.proc_write = gt91xx_config_write_proc,
 };
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
