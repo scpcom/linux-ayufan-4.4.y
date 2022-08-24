2acf940656144dac01e932a568a3bde41b37e595
scpcom <scpcom@gmx.de>
2022-08-25 00:04:16 +0200

    riscv: dma-mapping: remove dev parameter

--- arch/riscv/mm/dma-mapping.c
+++ arch/riscv/mm/dma-mapping.c
@@ -31,8 +31,8 @@ static inline void cache_op(phys_addr_t paddr, size_t size,
 	fn(start, start + size);
 }
 
-void arch_sync_dma_for_device(struct device *dev, phys_addr_t paddr,
-			      size_t size, enum dma_data_direction dir)
+void arch_sync_dma_for_device(phys_addr_t paddr, size_t size,
+		enum dma_data_direction dir)
 {
 	switch (dir) {
 	case DMA_TO_DEVICE:
@@ -47,8 +47,8 @@ void arch_sync_dma_for_device(struct device *dev, phys_addr_t paddr,
 	}
 }
 
-void arch_sync_dma_for_cpu(struct device *dev, phys_addr_t paddr,
-			   size_t size, enum dma_data_direction dir)
+void arch_sync_dma_for_cpu(phys_addr_t paddr, size_t size,
+		enum dma_data_direction dir)
 {
 	switch (dir) {
 	case DMA_TO_DEVICE:
