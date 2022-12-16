c9e4529c250b71de036ecbff5d90a0b0d8292cd2
scpcom <scpcom@gmx.de>
2022-12-16 02:22:35 +0100

    ODROID: ov5647: set type to V4L2_MBUS_CSI2_DPHY

--- drivers/media/i2c/ov5647.c
+++ drivers/media/i2c/ov5647.c
@@ -713,7 +713,7 @@ static int ov5647_g_mbus_config(struct v4l2_subdev *sd, unsigned int pad_id,
 	val = 1 << (OV5647_LANES - 1) |
 		V4L2_MBUS_CSI2_CHANNEL_0 |
 		V4L2_MBUS_CSI2_CONTINUOUS_CLOCK;
-	config->type = V4L2_MBUS_CSI2;
+	config->type = V4L2_MBUS_CSI2_DPHY;
 	config->flags = val;
 
 	return 0;
