8c0c093a116e40f0815d6a76c0cb04d7c7ca5f07
scpcom <scpcom@gmx.de>
2023-04-16 21:29:04 +0200

    fix merge "riscv: Bump COMMAND_LINE_SIZE value to 1024"

--- arch/riscv/include/uapi/asm/setup.h
+++ arch/riscv/include/uapi/asm/setup.h
@@ -1,27 +1,8 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-/*
- * Based on arch/arm/include/asm/setup.h
- *
- * Copyright (C) 1997-1999 Russell King
- * Copyright (C) 2012 ARM Ltd.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-#ifndef __ASM_SETUP_H
-#define __ASM_SETUP_H
+/* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
 
-#include <linux/types.h>
+#ifndef _UAPI_ASM_RISCV_SETUP_H
+#define _UAPI_ASM_RISCV_SETUP_H
 
 #define COMMAND_LINE_SIZE	2048
 
-#endif
+#endif /* _UAPI_ASM_RISCV_SETUP_H */
