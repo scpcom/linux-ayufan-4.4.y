596436f5724be8531689cb2834aafbd64d3c844c
scpcom <scpcom@gmx.de>
2023-07-26 00:21:24 +0200

    pfe: remove 'fallback' argument from dev->ndo_select_queue()

--- drivers/pfe_ctrl/pfe_eth.c
+++ drivers/pfe_ctrl/pfe_eth.c
@@ -2130,7 +2130,11 @@ static int pfe_eth_send_packet(struct sk_buff *skb, struct net_device *dev)
 /** pfe_eth_select_queue
  *
  */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,2,0)
+static u16 pfe_eth_select_queue(struct net_device *dev,
+						    struct sk_buff *skb,
+						    struct net_device *sb_dev)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0)
 static u16 pfe_eth_select_queue( struct net_device *dev, struct sk_buff *skb,
 		void *accel_priv, select_queue_fallback_t fallback)
 #else
