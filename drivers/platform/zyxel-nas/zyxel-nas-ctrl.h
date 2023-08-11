f7dc399333d6111a608a54b005845a460e736020
scpcom <scpcom@gmx.de>
2023-08-11 22:09:14 +0200

    platform: zyxel-nas: Add NAS Control

---
+++ drivers/platform/zyxel-nas/zyxel-nas-ctrl.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2023 SCP
+ *
+ *  Based on Zyxel NAS540 vendor kernel.
+ */
+
+#ifndef __ZYXEL_NAS_CTRL_H__
+#define __ZYXEL_NAS_CTRL_H__
+
+/* define the magic number for ioctl used */
+#define BTNCPY_IOC_MAGIC 'g'
+
+#define JIFFIES_1_SEC       (HZ)        /* 1 second */
+
+#define TIMER_RUNNING   0x1
+#define TIMER_SLEEPING  0x0
+
+struct nas_ctrl_timer_list {
+	struct timer_list timer;
+	void (*function)(unsigned long);
+	unsigned long data;
+};
+
+void nas_ctrl_init_timer(struct nas_ctrl_timer_list *ptimer, void *pfunc, unsigned long cntx);
+int nas_ctrl_mod_timer(struct nas_ctrl_timer_list *ptimer, unsigned long expires);
+int nas_ctrl_del_timer(struct nas_ctrl_timer_list * ptimer);
+int nas_ctrl_del_timer_sync(struct nas_ctrl_timer_list *ptimer);
+int nas_ctrl_timer_pending(const struct nas_ctrl_timer_list *ptimer);
+int run_usermode_cmd(const char *cmd);
+
+void Beep(void);
+void Beep_Beep(int duty_high, int duty_low);
+
+#endif
