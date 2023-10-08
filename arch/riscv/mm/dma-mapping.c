ccb01329c54987d2d376609adeff843e1d35b0c3
scpcom <scpcom@gmx.de>
2023-10-08 18:19:39 +0200

    riscv: dma-mapping: drop the dev argument to arch_sync_dma_for_*

--- arch/riscv/mm/dma-mapping.c
+++ arch/riscv/mm/dma-mapping.c
@@ -31,7 +31,7 @@ static inline void cache_op(phys_addr_t paddr, size_t size,
 	fn(start, start + size);
 }
 
-void arch_sync_dma_for_device(struct device *dev, phys_addr_t paddr,
+void arch_sync_dma_for_device(phys_addr_t paddr,
 			      size_t size, enum dma_data_direction dir)
 {
 	switch (dir) {
@@ -47,7 +47,7 @@ void arch_sync_dma_for_device(struct device *dev, phys_addr_t paddr,
 	}
 }
 
-void arch_sync_dma_for_cpu(struct device *dev, phys_addr_t paddr,
+void arch_sync_dma_for_cpu(phys_addr_t paddr,
 			   size_t size, enum dma_data_direction dir)
 {
 	switch (dir) {
