743654fe6c41d2e15fbb80ebdda0433f5d4f8926
scpcom <scpcom@gmx.de>
2023-11-22 02:45:51 +0100

    arm: mach-comcerto: smp: remove holding pen

--- arch/arm/mach-comcerto/platsmp.c
+++ arch/arm/mach-comcerto/platsmp.c
@@ -103,55 +103,10 @@ void __init platform_smp_prepare_cpus(unsigned int max_cpus)
 	return;
 }
 
-/*
- * control for which core is the next to come out of the secondary
- * boot "holding pen"
- */
-//volatile int __cpuinitdata pen_release = -1; /* MAK: FIXME */
-extern volatile int pen_release;
-
-/*
- * Write pen_release in a way that is guaranteed to be visible to all
- * observers, irrespective of whether they're taking part in coherency
- * or not.  This is necessary for the hotplug code to work reliably.
- */
-static void __cpuinit write_pen_release(int val)
-{
-	pen_release = val;
-	smp_wmb();
-	__cpuc_flush_dcache_area((void *)&pen_release, sizeof(pen_release));
-	outer_clean_range(__pa(&pen_release), __pa(&pen_release + 1));
-}
-
 static DEFINE_SPINLOCK(boot_lock);
-
-void __cpuinit platform_secondary_init(unsigned int cpu)
-{
-	/*
-	 * if any interrupts are already enabled for the primary
-	 * core (e.g. timer irq), then they will not have been enabled
-	 * for us: do so
-	 */
-	// gic_secondary_init(0); /* MAK: FIXME */
-
-	/*
-	 * let the primary processor know we're out of the
-	 * pen, then head off into the C entry point
-	 */
-	write_pen_release(-1);
-
-	/*
-	 * Synchronise with the boot thread.
-	 */
-	spin_lock(&boot_lock);
-	spin_unlock(&boot_lock);
-}
 
 int __cpuinit platform_boot_secondary(unsigned int cpu, struct task_struct *idle)
 {
-	unsigned long timeout;
-	unsigned int *loop = (unsigned int *)phys_to_virt(0x08);
-
 	/*
 	 * Install the comcerto_secondary_startup pointer at 0x20
 	 * Physical Address
@@ -205,42 +160,15 @@ int __cpuinit platform_boot_secondary(unsigned int cpu, struct task_struct *idle
 	 * and the secondary one
 	 */
 	spin_lock(&boot_lock);
-
-	/*
-	 * This is really belt and braces; we hold unintended secondary
-	 * CPUs in the holding pen until we're ready for them.  However,
-	 * since we haven't sent them a soft interrupt, they shouldn't
-	 * be there.
-	 */
-	write_pen_release(cpu);
-
-	/*
-	 * Send the secondary CPU a soft interrupt, thereby causing
-	 * the boot monitor to read the system wide flags register,
-	 * and branch to the address found there.
-	 */
-	timeout = jiffies + (1 * HZ);
-	while (time_before(jiffies, timeout)) {
-		smp_rmb();
-		if (pen_release == -1)
-			break;
-
-		udelay(10);
-	}
-
-	/*
-	 * now the secondary core is starting up let it run its
-	 * calibrations, then wait for it to finish
-	 */
 	spin_unlock(&boot_lock);
 
-	return pen_release != -1 ? -ENOSYS : 0;
+	return 0;
 }
 
 struct smp_operations comcerto_smp_ops __initdata = {
     .smp_init_cpus      = platform_smp_init_cpus,
     .smp_prepare_cpus   = platform_smp_prepare_cpus,
-    .smp_secondary_init = platform_secondary_init,
+    //.smp_secondary_init = platform_secondary_init,
     .smp_boot_secondary = platform_boot_secondary,
 #ifdef CONFIG_HOTPLUG_CPU
     .cpu_die        = platform_cpu_die,
