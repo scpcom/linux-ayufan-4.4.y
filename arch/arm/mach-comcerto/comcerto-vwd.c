7b8888dca9f21b1f674d39b7958a44f73224d713
scpcom <scpcom@gmx.de>
2023-11-24 18:35:15 +0100

    arm: mach-comcerto: vwd: include skbuff.h

--- arch/arm/mach-comcerto/comcerto-vwd.c
+++ arch/arm/mach-comcerto/comcerto-vwd.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <linux/module.h>
+#include <linux/skbuff.h>
 
 
 //static int comcerto_wifi_rx_dummy_hdlr(struct sk_buff *skb);
