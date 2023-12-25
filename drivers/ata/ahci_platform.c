6dff3e6ca7f8ac2f2814be3fb42fb6923ea5df8a
scpcom <scpcom@gmx.de>
2023-12-25 17:24:37 +0100

    ahci: ls1024a: Set the SATA PMU clock to 30 MHZ and OOB clock to 125MHZ

--- drivers/ata/ahci_platform.c
+++ drivers/ata/ahci_platform.c
@@ -56,6 +56,18 @@ static int ahci_probe(struct platform_device *pdev)
 	if (rc)
 		return rc;
 
+#ifdef CONFIG_ARCH_LS1024A
+	if (of_device_is_compatible(dev->of_node, "fsl,ls1024a-sata")) {
+		struct clk *sata_oob_clk = ahci_platform_find_clk(hpriv, "oob");
+		struct clk *sata_pmu_clk = ahci_platform_find_clk(hpriv, "pmu");
+
+		/* Set the SATA PMU clock to 30 MHZ and OOB clock to 125MHZ */
+		dev_info(dev, "Set the SATA PMU clock to 30 MHZ and OOB clock to 125MHZ\n");
+		clk_set_rate(sata_oob_clk,125000000);
+		clk_set_rate(sata_pmu_clk,30000000);
+	}
+#endif
+
 	if (of_device_is_compatible(dev->of_node, "hisilicon,hisi-ahci"))
 		hpriv->flags |= AHCI_HFLAG_NO_FBS | AHCI_HFLAG_NO_NCQ;
 
