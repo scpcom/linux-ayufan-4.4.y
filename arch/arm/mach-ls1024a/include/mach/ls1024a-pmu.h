d57bc3024150ca2ba882a1ee37a3b5b23b5e7540
scpcom <scpcom@gmx.de>
2024-01-20 21:00:22 +0100

    ARM: ls1024a: Rename the shared pmu IRQs

--- arch/arm/mach-ls1024a/include/mach/ls1024a-pmu.h
+++ arch/arm/mach-ls1024a/include/mach/ls1024a-pmu.h
@@ -3,16 +3,16 @@
 
 #define HOST_UTILPE_COMPATIBLE "fsl,ls1024a-host-utilpe"
 
-#define PCIe0_IRQ               (1 << 15)
+#define LS1024A_PMU_PCIe0_IRQ               (1 << 15)
 
-#define PCIe1_IRQ               (1 << 16)
-#define SATA_IRQ                (1 << 17)
-#define SATA_MSI_IRQ            (1 << 18)
-#define USB2p0_IRQ              (1 << 19)
+#define LS1024A_PMU_PCIe1_IRQ               (1 << 16)
+#define LS1024A_PMU_SATA_IRQ                (1 << 17)
+#define LS1024A_PMU_SATA_MSI_IRQ            (1 << 18)
+#define LS1024A_PMU_USB2p0_IRQ              (1 << 19)
 
-#define USB3p0_IRQ              (1 << 20)
-#define HFE_0_IRQ               (1 << 21)
-#define WOL_IRQ                 (1 << 22)
+#define LS1024A_PMU_USB3p0_IRQ              (1 << 20)
+#define LS1024A_PMU_HFE_0_IRQ               (1 << 21)
+#define LS1024A_PMU_WOL_IRQ                 (1 << 22)
 
 extern unsigned host_utilpe_shared_pmu_bitmask;
 
