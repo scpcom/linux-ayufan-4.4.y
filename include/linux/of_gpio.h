844afe195e2ece1a2ec20c13cec3dac00bdcc561
scpcom <scpcom@gmx.de>
2024-02-09 01:26:41 +0100

    Revert "gpiolib: of: Move enum of_gpio_flags to its only user"
    This reverts commit eed5a3bfafe6840494f7752b5cecd2a610b54fef.

--- include/linux/of_gpio.h
+++ include/linux/of_gpio.h
@@ -17,6 +17,21 @@
 
 struct device_node;
 
+/*
+ * This is Linux-specific flags. By default controllers' and Linux' mapping
+ * match, but GPIO controllers are free to translate their own flags to
+ * Linux-specific in their .xlate callback. Though, 1:1 mapping is recommended.
+ */
+enum of_gpio_flags {
+	OF_GPIO_ACTIVE_LOW = 0x1,
+	OF_GPIO_SINGLE_ENDED = 0x2,
+	OF_GPIO_OPEN_DRAIN = 0x4,
+	OF_GPIO_TRANSITORY = 0x8,
+	OF_GPIO_PULL_UP = 0x10,
+	OF_GPIO_PULL_DOWN = 0x20,
+	OF_GPIO_PULL_DISABLE = 0x40,
+};
+
 #ifdef CONFIG_OF_GPIO
 
 extern int of_get_named_gpio(const struct device_node *np,
