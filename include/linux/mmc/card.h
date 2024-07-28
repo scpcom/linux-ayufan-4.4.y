7c580a65566eafd36ec259cbaa054087f98d3b40
scpcom <scpcom@gmx.de>
2024-07-28 15:51:24 +0000

    fix merge "mmc: core: Add SD card quirk for broken discard"

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -270,7 +270,8 @@ struct mmc_card {
 #define MMC_QUIRK_BROKEN_IRQ_POLLING	(1<<11)	/* Polling SDIO_CCCR_INTx could create a fake interrupt */
 #define MMC_QUIRK_TRIM_BROKEN	(1<<12)		/* Skip trim */
 #define MMC_QUIRK_BROKEN_HPI	(1<<13)		/* Disable broken HPI support */
-#define MMC_QUIRK_EXTRA_LONG_READ_TIME (1<<14)	/* For Micron eMMC need very long read time after power loss boot */
+#define MMC_QUIRK_BROKEN_SD_DISCARD	(1<<14)	/* Disable broken SD discard support */
+#define MMC_QUIRK_EXTRA_LONG_READ_TIME (1<<15)	/* For Micron eMMC need very long read time after power loss boot */
 
 	bool			reenable_cmdq;	/* Re-enable Command Queue */
 
